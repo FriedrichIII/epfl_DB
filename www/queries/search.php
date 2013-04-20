bf1b25adc2d257a8f2287e5184937931d405b74e
antoine <antoine.morand@epfl.ch>
2013-04-20 12:18:00 +0200

    text search: single request + match score
    includes preparation of details page

--- www/queries/search.php
+++ www/queries/search.php
@@ -1,166 +1,218 @@
-<h1>Search results</h1>
-<p>for: "<?php echo(str_replace('<', '{', $_GET['search_field']))?>"</p>
+
+<head>
+<style type="text/css">
+p{margin:0;
+}
+</style>
+</head>
+
+
+<?php
+$origTxt = $_GET['search_field'];
+
+$txt = '%' . $origTxt . '%';
+
+$pattern = '/[ \n\t]/';
+$terms = preg_split($pattern, $origTxt);
+$termsCount = count($terms);
+?>
+
+<h1>Results for "<?php echo(str_replace('<', '{', $origTxt))?>"</h1>
+
 <?php
 try
 {
 	$db = new PDO('mysql:host=localhost;dbname=olympics', 'root', '');
-
 	
-	
-	// ATHLETES
+	$paramsList = array();
 	$qry = $db->prepare('
 	
-	SELECT
-		name
-	FROM
-		athletes
-	WHERE
-		name LIKE ?;
+SELECT id, type, text, matches FROM
+(
 	
-	');
-	$qry->execute(array('%' . $_GET['search_field'] . '%'));
-	echo('<h3>Athletes</h3>');
-	while($data = $qry->fetch())
-	{
-		echo($data['name'] . "<br />");
-	}
+	/* A T H L E T E S */
 	
-	
+	SELECT aID AS id, \'a\' AS type, CONCAT(aname, \' (\' ,code , \')\') AS text, COUNT(*) AS matches FROM
+	(
+		SELECT aID, aname, code
+		FROM aview
+		WHERE ' . buildConds('aname', $terms, $paramsList) . '
+			
+		UNION ALL
 		
-	// CITIES
-	$qry = $db->prepare('
+		SELECT aID, aname, code
+		FROM aview
+		WHERE ' . buildConds('coname', $terms, $paramsList) . '
+	) am
+	GROUP BY aID
 	
-	SELECT
-		name
-	FROM
-		cities
-	WHERE
-		name LIKE ?;
-	
-	');
-	$qry->execute(array('%' . $_GET['search_field'] . '%'));
-	echo('<h3>Cities</h3>');
-	while($data = $qry->fetch())
-	{
-		echo($data['name'] . "<br />");
-	}
+	UNION ALL
 	
 	
+	/* C I T I E S */
+		
+	SELECT ciID as id, \'ci\' AS type, CONCAT(ciname, \' (\' ,code , \')\') AS text, COUNT(*) AS matches FROM
+	(
+		SELECT ciID, ciname, code
+		FROM ciview
+		WHERE ' . buildConds('ciname', $terms, $paramsList) . '
+		
+		UNION ALL
+		
+		SELECT ciID, ciname, code
+		FROM ciview
+		WHERE ' . buildConds('coname', $terms, $paramsList) . '
+	) cim
+	GROUP BY ciID
 	
-	// COUNTRIES
-	$qry = $db->prepare('
+	UNION ALL
+
+
+	/* C O U N T R I E S */
 	
-	SELECT
-		name
-	FROM
-		countries
-	WHERE
-		name LIKE ?;
+	SELECT code AS id, \'co\' AS type, CONCAT(coname, \' (\' ,code , \')\') AS text, COUNT(*) AS matches FROM
+	(
+		SELECT co.name AS coname, co.iocCode AS code
+		FROM countries co
+		WHERE ' . buildConds('co.name', $terms, $paramsList) . '
+		
+		UNION ALL
+		
+		SELECT co.name AS coname, co.iocCode AS code
+		FROM countries co
+		WHERE ' . buildConds('co.iocCode', $terms, $paramsList) . '
+	) com
+	GROUP BY code	
 	
-	');
-	$qry->execute(array('%' . $_GET['search_field'] . '%'));
-	echo('<h3>Countries</h3>');
-	while($data = $qry->fetch())
-	{
-		echo($data['name'] . "<br />");
-	}
-	
-	
-	
-	// DISCIPLINES
-	$qry = $db->prepare('
-	
-	SELECT
-		d.name AS dname, s.name AS sname
-	FROM
-		disciplines d INNER JOIN sports s
-		ON d.sportID = s.sportID
-	WHERE 
-		d.name LIKE ? OR s.name LIKE ?;
-	
-	');
-	$qry->execute(array('%' . $_GET['search_field'] . '%', '%' . $_GET['search_field'] . '%'));
-	echo('<h3>Disciplines</h3>');
-	while($data = $qry->fetch())
-	{
-		echo($data['sname'] . ",  " . $data['dname'] . "<br />");
-	}
-	
-	
-	
-	// EVENTS
-	$qry = $db->prepare('
-	
-	SELECT
-		name
-	FROM
-		events
-	WHERE
-		name LIKE ?;
-	
-	');
-	$qry->execute(array('%' . $_GET['search_field'] . '%'));
-	echo('<h3>Events</h3>');
-	while($data = $qry->fetch())
-	{
-		echo($data['name'] . "<br />");
-	}
-	
-	
-	
-	// GAMES
-	/*
-	 * Search on season, year and city
-	 */
-	$qry = $db->prepare('
-	
-	SELECT
-		g.seasonName, g.year, c.name
-	FROM
-		games g INNER JOIN cities c
-		ON c.cityID = g.cityID
-	WHERE 
-		g.seasonName LIKE ? OR g.year LIKE ? OR c.name LIKE ?
-	ORDER BY
-		g.year;
-	
-	');
-	$qry->execute(array('%' . $_GET['search_field'] . '%', '%' . $_GET['search_field'] . '%', '%' . $_GET['search_field'] . '%'));
-	echo('<h3>Games</h3>');
-	while($data = $qry->fetch())
-	{
-		echo($data['seasonName'] . " games of " . $data['name'] . " " . $data['year'] . "<br />");
-	}
-	
-	
-	
-	// SPORTS
-	$qry = $db->prepare('
-	
-	SELECT
-		name
-	FROM
-		Sports
-	WHERE
-		name LIKE ?;
-	
-	');		
-	$qry->execute(array('%' . $_GET['search_field'] . '%'));
-	echo('<h3>Sports</h3>');
-	while($data = $qry->fetch())
-	{
-		echo($data['name'] . "<br />");
-	}
-	
+	UNION ALL
 
 	
-	// TODO special medal search
+	/* D I S C I P L I N E S */
+
+	SELECT dID AS id, \'d\' AS type, CONCAT(sname, \' \', dname) AS text, COUNT(*) AS matches FROM
+	(
+		SELECT dID, dname, sname
+		FROM dview
+		WHERE ' . buildConds('dname', $terms, $paramsList) . '
+		
+		UNION ALL
+		
+		SELECT dID, dname, sname
+		FROM dview
+		WHERE ' . buildConds('sname', $terms, $paramsList) . '
+	) dm
+	GROUP BY dID
 	
+	UNION ALL
+	
+	
+	/* S P O R T S */
+
+	SELECT sID AS id, \'s\' AS type, sname AS text, COUNT(*) AS matches FROM
+	(
+		SELECT s.sportID AS sID, s.name AS sname
+		FROM sports s
+		WHERE ' . buildConds('s.name', $terms, $paramsList) . '
+	) sm
+	GROUP BY sID
+	
+	UNION ALL
+	
+	
+	/* E V E N T S */
+	
+	SELECT eID AS id, \'e\' AS type, CONCAT(sname, \' \', dname, \' \', ename, \' \', year) AS text, COUNT(*) AS matches FROM
+	(
+		SELECT eID, ename, sname, dname, ciname, year
+		FROM eview
+		WHERE ' . buildConds('ename', $terms, $paramsList) . '
+		
+		UNION ALL
+		
+		SELECT eID, ename, sname, dname, ciname, year
+		FROM eview
+		WHERE ' . buildConds('sname', $terms, $paramsList) . '		
+		
+		UNION ALL
+		
+		SELECT eID, ename, sname, dname, ciname, year
+		FROM eview
+		WHERE ' . buildConds('dname', $terms, $paramsList) . '
+		
+		UNION ALL
+		
+		SELECT eID, ename, sname, dname, ciname, year
+		FROM eview
+		WHERE ' . buildConds('year', $terms, $paramsList) . ' OR ' . buildConds('ciname', $terms, $paramsList) . '
+	) em
+	GROUP BY eID
+	
+	UNION ALL
+	
+	
+	/* G A M E S */
+	SELECT gID as id, \'g\' AS type, CONCAT(ciname, \' \', year, \' Games\') AS text, COUNT(*) AS matches FROM
+	(
+		SELECT gID, ciname, year
+		FROM gview
+		WHERE ' . buildConds('ciname', $terms, $paramsList) . '
+		
+		UNION ALL
+		
+		SELECT gID, ciname, year
+		FROM gview
+		WHERE ' . buildConds('year', $terms, $paramsList) . '
+	) gm
+	GROUP BY gID
+	
+) found
+ORDER BY matches DESC;
+
+	');
+	
+	bindParams($qry, $paramsList);
+	$qry->execute();
+
+	$title = false;
+	while($data = $qry->fetch())
+	{
+		$percent = intval(min(100, $data['matches'] / $termsCount * 100));
+		echo("<p style=\"font-size:" . 3 * sqrt($percent) . "px\" topmargin=\"0\">" . $data['text'] . " - <i>" . $percent . "%</i></p>");
+	}
+
+
+// TODO special medal search
+
 
 
 }
 catch (Exception $e)
 {
 	die('Could not open database, error: ' . $e->getMessage());
+}
+
+/*
+ * This function builds alternative conditions in case of a multiple term search.
+ */
+function buildConds($field, $terms, &$paramsList) {
+	
+	$conds = array();
+	foreach ($terms as $term) {
+		array_push($conds, ' LCASE('. $field . ') LIKE LCASE(?)');
+		array_push($paramsList, '%' . $term . '%');
+	}
+		
+	return implode(" OR ", $conds);
+}
+
+/*
+ * This function binds the gathered values to a request.
+ */
+function bindParams($req, $paramsList) {
+	$i = 1;
+	foreach ($paramsList as $param) {
+		$req->bindValue($i, $param, PDO::PARAM_STR);
+		$i++;
+	}
 }
 ?>
