8a214be818ee7d92f495c7b13e941ce1959ec54e
antoine <antoine.morand@epfl.ch>
2013-04-21 22:06:12 +0200

    added config for db info (still to do special queries)

--- www/queries/details-g.php
+++ www/queries/details-g.php
@@ -1,7 +1,8 @@
 
 <?php
-$id =  $_GET['id'];
-$db = new PDO('mysql:host=localhost;dbname=olympics', 'root', '');
+include 'config.php';
+$id = $_GET['id'];
+$db = new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME, $DATABASE_LOGIN, $DATABASE_PASSWORD);
 
 $qry = $db->prepare('
 SELECT ci.name AS ciname, ci.cityID AS ciID, g.year AS year, g.seasonName AS season, co.name AS coname, co.iocCode AS iocCode
