52c120be1cbba78e854f91da17d3a5e15bd17c9d
antoine <antoine.morand@epfl.ch>
2013-04-22 00:34:18 +0200

    gold medalist (B) fixed

--- www/queries/goldMedalists.php
+++ www/queries/goldMedalists.php
@@ -12,7 +12,13 @@ JOIN Teams t ON m.teamID = t.teamID
 JOIN Events e ON t.eventID = e.eventID
 JOIN Disciplines d ON e.disciplineID = d.disciplineID
 JOIN Sports s ON d.sportID = s.sportID
-WHERE t.rank = 1 
+WHERE t.rank = 1 AND s.sportID IN
+	(SELECT s2.sportID
+	FROM Events e2
+	JOIN Disciplines d2 ON e2.disciplineID = d2.disciplineID
+	JOIN Sports s2 ON d2.sportID = s2.sportID
+	GROUP BY s2.sportID
+	HAVING COUNT(DISTINCT e2.gameID) = 1)
 	');
 	$qry->execute();
 	while($data = $qry->fetch())
