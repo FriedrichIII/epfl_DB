fc22f89ea796aa294a97418905da678159017103
antoine <antoine.morand@epfl.ch>
2013-04-21 21:09:38 +0200

    games detail + some tweaks

--- www/queries/details-g.php
+++ www/queries/details-g.php
@@ -4,35 +4,43 @@ $id =  $_GET['id'];
 $db = new PDO('mysql:host=localhost;dbname=olympics', 'root', '');
 
 $qry = $db->prepare('
-SELECT ci.name AS ciname, g.year AS year
+SELECT ci.name AS ciname, ci.cityID AS ciID, g.year AS year, g.seasonName AS season, co.name AS coname, co.iocCode AS iocCode
 FROM games g
 INNER JOIN cities ci ON g.cityID = ci.cityID
+INNER JOIN countries co ON co.iocCode = ci.iocCode
 WHERE g.gameID = \'' . $id . '\'
 	');
 
 $qry->execute();
 $res = $qry->fetch();
 $qry->fetchAll();
-$name = $res['ciname'] . ' ' . $res['year'] ;
 ?>
 
-<h1>Details for Olympic Games "<?php echo($name)?>":</h1>
+<h1>Details for Olympic Games "<?php echo($res['ciname'] . ' ' . $res['year'])?>":</h1>
+
+<h3>Season:</h3>
+<?php echo($res['season'])?>
+<h3>Place:</h3>
+<?php echo('<a href="details-ci.php?id=' . $res['ciID'] . '">' . $res['ciname'] . '</a>, '
+ . '<a href="details-co.php?id=' . $res['iocCode'] . '">' . $res['coname']. '</a>')?>
 
 
-<h3>Medalists:</h3>
+<h3>Events / Medalists:</h3>
 
 <?php
 $medals = array(1 => 'Gold', 2 => 'Silver', 3 => 'Bronze');
 
 $qry = $db->prepare('	
-SELECT a.name AS aname, a.athleteID AS aID, t.rank AS rank
+SELECT e.eventID as eID, a.athleteID as aID, a.name AS aname, t.rank AS rank, d.name AS dname, s.name AS sname, e.name AS ename
 FROM games g
 INNER JOIN events e ON e.gameID = g.gameID
 INNER JOIN teams t ON t.eventID = e.eventID
 INNER JOIN memberships m ON m.teamID = t.teamID
 INNER JOIN athletes a ON a.athleteID = m.athleteID
-WHERE e.eventID = \'' . $id . '\' AND t.rank < 4
-ORDER BY t.rank
+INNER JOIN disciplines d ON d.disciplineID = e.disciplineID
+INNER JOIN sports s ON s.sportID = d.disciplineID
+WHERE g.gameID = \'' . $id . '\' AND t.rank < 4
+ORDER BY s.name, d.name, t.rank, a.name
 ');
 
 
@@ -40,19 +48,21 @@ $qry->execute();
 
 $res = $qry->fetchAll();
 $size = count($res);
-$prevRank = -1;
+$prevEID = -1;
 
 for ($i = 0; $i < $size; $i++) {
 	$r = $res[$i];
 	
 	// rank
-	if ($prevRank < $r['rank']) {
-		$prevRank = $r['rank'];
-		echo('<b>' . $medals[$r['rank']] . '</b><br />');
+	if ($prevEID != $r['eID']) {
+		$prevEID = $r['eID'];
+		echo('<a href="details-e.php?id="' . $r['eID'] . '"><b>' . $r['sname'] .
+		 ' ' . $r['dname'] . ' ' . $r['ename'] . '</b><br />');
 	}
 	
 	// Athlete
-	echo('<a href="details-a.php?id=' . $r['aID'] .  '">' . $r['aname'] . '</a><br />');
+	echo('<a href="details-a.php?id=' . $r['aID'] .  '">' . $r['aname'] .
+	 ' ('  . $medals[$r['rank']] . ')</a><br />');
 }
 
 $db = null;
