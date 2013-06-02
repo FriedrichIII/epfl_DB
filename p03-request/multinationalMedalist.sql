7955f4b33b0b404a763ec4cd80861c1283ad6418
Valerian Pittet <valerian.pittet@epfl.ch>
2013-06-02 19:00:57 +0200

    multinationalMedalist.sql done

---
+++ p03-request/multinationalMedalist.sql
@@ -0,0 +1,8 @@
+-- List all Olympians who won medals for multiple nations.
+SELECT Ath.name
+FROM athletes Ath
+JOIN memberships Me ON Ath.athleteID = Me.athleteID
+JOIN teams Te ON Me.teamID = Te.teamID
+WHERE Te.rank < 4 AND Te.rank > 0
+GROUP BY Ath.athleteID
+HAVING COUNT (DISTINCT Te.ioccode) > 1
