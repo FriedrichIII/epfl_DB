98eecfdbe7bda8dda5ecb36560c5b82b37d53f1a
Valerian Pittet <valerian.pittet@epfl.ch>
2013-06-03 01:11:48 +0200

    rapport

--- p03-request/multinationalMedalist.sql
+++ p03-request/multinationalMedalist.sql
@@ -5,4 +5,4 @@ JOIN memberships Me ON Ath.athleteID = Me.athleteID
 JOIN teams Te ON Me.teamID = Te.teamID
 WHERE Te.rank < 4 AND Te.rank > 0
 GROUP BY Ath.athleteID
-HAVING COUNT (DISTINCT Te.ioccode) > 1
+HAVING COUNT(DISTINCT Te.ioccode) > 1
