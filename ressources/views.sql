4b34527076e89c8a60f85ebeb1429ebcf346e871
FriedrichIII <pascal.cudre@gmail.com>
2013-06-03 04:09:19 +0200

    new dump

--- ressources/views.sql
+++ ressources/views.sql
@@ -1,4 +1,4 @@
-﻿CREATE VIEW aview AS
+﻿CREATE OR REPLACE VIEW aview AS
 SELECT
 	a.athleteID AS aID, a.name AS aname, co.name AS coname, t.iocCode AS code
 FROM athletes a
@@ -6,19 +6,19 @@ INNER JOIN memberships m ON a.athleteID = m.athleteID
 INNER JOIN teams t ON t.teamID = m.teamID
 INNER JOIN countries co ON t.iocCode = co.iocCode;
 
-CREATE VIEW ciview AS
+CREATE OR REPLACE VIEW ciview AS
 SELECT
 	ci.cityID AS ciID, ci.name AS ciname, co.name AS coname, co.iocCode AS code
 FROM cities ci
 INNER JOIN countries co ON ci.iocCode = co.iocCode;
 
-CREATE VIEW dview AS
+CREATE OR REPLACE VIEW dview AS
 SELECT
 	d.disciplineID AS dID, d.name AS dname, s.name AS sname
 FROM disciplines d
 INNER JOIN sports s ON d.sportID = s.sportID;
 
-CREATE VIEW eview AS
+CREATE OR REPLACE VIEW eview AS
 SELECT
 	e.eventID AS eID, e.name AS ename, d.name AS dname, s.name AS sname, g.cityID as ciname, g.year as year
 FROM events e
@@ -26,7 +26,7 @@ INNER JOIN disciplines d ON e.disciplineID = d.disciplineID
 INNER JOIN sports s ON d.sportID = s.sportID
 INNER JOIN games g ON e.gameID = g.gameID;
 
-CREATE VIEW gview AS
+CREATE OR REPLACE VIEW gview AS
 SELECT
 	g.gameID AS gID, ci.name AS ciname, g.year as year
 FROM games g
