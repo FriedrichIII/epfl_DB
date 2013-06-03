fa47cb15ada7b990a30ad52a2d6d7ca0d4bc412a
Valerian Pittet <valerian.pittet@epfl.ch>
2013-06-03 02:12:06 +0200

    +1

--- App/webapp/queries/eventsMedalIn1Country.php
+++ App/webapp/queries/eventsMedalIn1Country.php
@@ -1,10 +1,30 @@
--- List all events for which all medals are won by athletes from the same country
-SELECT Ev.name
-FROM events Ev
-JOIN 	(SELECT Ev1.eventId
-	FROM events Ev1
-	JOIN teams Te ON Ev1.eventID = Te.eventID
-	WHERE Te.rank < 4 AND Te.rank > 0
-	GROUP BY Ev1.eventID
-	HAVING COUNT(DISTINCT Te.ioccode) = 1) EvIds
-ON Ev.eventID = EvIds.eventID
+<h1>First Medal Places by Country</h1>
+<?php
+try
+{
+	include 'config.php';
+	$db = new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME, $DATABASE_LOGIN, $DATABASE_PASSWORD);
+	$qry = $db->prepare('
+	SELECT Ev.name AS eventName
+	FROM events Ev
+	JOIN 	(SELECT Ev1.eventId
+		FROM events Ev1
+		JOIN teams Te ON Ev1.eventID = Te.eventID
+		WHERE Te.rank < 4 AND Te.rank > 0
+		GROUP BY Ev1.eventID
+		HAVING COUNT(DISTINCT Te.ioccode) = 1) EvIds
+	ON Ev.eventID = EvIds.eventID
+	
+	');
+	$qry->execute();
+	
+	while($data = $qry->fetch())
+	{
+		echo($data['eventName'] . ')<br />');
+	}
+}
+catch (Exception $e)
+{
+	die('Could not open database, error: ' . $e->getMessage());
+}
+?>
